bc4c43f5f96fd5c0b30f7ba18221028661413a94
Adrian Gschwend <ktk@netlabs.org>
2017-01-20 17:31:43 +0100

    Enforce ntriples output

--- scripts/rml.sh
+++ scripts/rml.sh
@@ -1,20 +1,20 @@
-java -jar lib/RML-Processor-0.3x100.jar -m config/production_mapping_Datenbankurs_Amateurtheater.ttl -o target/mapping_01.nt
-java -jar lib/RML-Processor-0.3x100.jar -m config/production_mapping_Datenbankurs_FesteHaeuser.ttl -o target/mapping_02.nt
-java -jar lib/RML-Processor-0.3x100.jar -m config/production_mapping_Datenbankurs_Festivals.ttl -o target/mapping_03.nt
-java -jar lib/RML-Processor-0.3x100.jar -m config/production_mapping_Datenbankurs_Festspiele.ttl -o target/mapping_04.nt
-java -jar lib/RML-Processor-0.3x100.jar -m config/production_mapping_Datenbankurs_Foerderverein.ttl -o target/mapping_05.nt
-java -jar lib/RML-Processor-0.3x100.jar -m config/production_mapping_Datenbankurs_Freilichtspiele.ttl -o target/mapping_06.nt
-java -jar lib/RML-Processor-0.3x100.jar -m config/production_mapping_Datenbankurs_Museen.ttl -o target/mapping_07.nt
-java -jar lib/RML-Processor-0.3x100.jar -m config/production_mapping_Datenbankurs_ProduzentenTraegerAgenturen.ttl -o target/mapping_08.nt
-java -jar lib/RML-Processor-0.3x100.jar -m config/production_mapping_Datenbankurs_Schulen.ttl -o target/mapping_09.nt
-java -jar lib/RML-Processor-0.3x100.jar -m config/production_mapping_Datenbankurs_TruppenProfi.ttl -o target/mapping_10.nt
-java -jar lib/RML-Processor-0.3x100.jar -m config/production_mapping_Datenbankurs_TruppenPuppentheater.ttl -o target/mapping_11.nt
-java -jar lib/RML-Processor-0.3x100.jar -m config/production_mapping_Datenbankurs_Verbaende.ttl -o target/mapping_12.nt
-java -jar lib/RML-Processor-0.3x100.jar -m config/production_mapping_Datenbankurs_VorfasnachtRevueKulturnaechte.ttl -o target/mapping_13.nt
-java -jar lib/RML-Processor-0.3x100.jar -m config/production_mapping_Datenbankurs_Zirkus.ttl -o target/mapping_14.nt
-java -jar lib/RML-Processor-0.3x100.jar -m config/production_mapping_Plakat_alleAttribute.ttl -o target/mapping_15.nt
-java -jar lib/RML-Processor-0.3x100.jar -m config/production_mapping_RepGesamt_alleAttributeohneDokumente.ttl -o target/mapping_16.nt
-java -jar lib/RML-Processor-0.3x100.jar -m config/production_mapping_StammdatenSpielzeit.ttl -o target/mapping_17.nt
-java -jar lib/RML-Processor-0.3x100.jar -m config/production_mapping_ZSV.ttl -o target/mapping_18.nt
-java -jar lib/RML-Processor-0.3x100.jar -m config/production_mapping_audio.ttl -o target/mapping_19.nt
-java -jar lib/RML-Processor-0.3x100.jar -m config/production_mapping_stc_professional.ttl -o target/mapping_20.nt
+java -jar lib/RML-Processor-0.3x100.jar -f ntriples -m config/production_mapping_Datenbankurs_Amateurtheater.ttl -o target/mapping_01.nt
+java -jar lib/RML-Processor-0.3x100.jar -f ntriples -m config/production_mapping_Datenbankurs_FesteHaeuser.ttl -o target/mapping_02.nt
+java -jar lib/RML-Processor-0.3x100.jar -f ntriples -m config/production_mapping_Datenbankurs_Festivals.ttl -o target/mapping_03.nt
+java -jar lib/RML-Processor-0.3x100.jar -f ntriples -m config/production_mapping_Datenbankurs_Festspiele.ttl -o target/mapping_04.nt
+java -jar lib/RML-Processor-0.3x100.jar -f ntriples -m config/production_mapping_Datenbankurs_Foerderverein.ttl -o target/mapping_05.nt
+java -jar lib/RML-Processor-0.3x100.jar -f ntriples -m config/production_mapping_Datenbankurs_Freilichtspiele.ttl -o target/mapping_06.nt
+java -jar lib/RML-Processor-0.3x100.jar -f ntriples -m config/production_mapping_Datenbankurs_Museen.ttl -o target/mapping_07.nt
+java -jar lib/RML-Processor-0.3x100.jar -f ntriples -m config/production_mapping_Datenbankurs_ProduzentenTraegerAgenturen.ttl -o target/mapping_08.nt
+java -jar lib/RML-Processor-0.3x100.jar -f ntriples -m config/production_mapping_Datenbankurs_Schulen.ttl -o target/mapping_09.nt
+java -jar lib/RML-Processor-0.3x100.jar -f ntriples -m config/production_mapping_Datenbankurs_TruppenProfi.ttl -o target/mapping_10.nt
+java -jar lib/RML-Processor-0.3x100.jar -f ntriples -m config/production_mapping_Datenbankurs_TruppenPuppentheater.ttl -o target/mapping_11.nt
+java -jar lib/RML-Processor-0.3x100.jar -f ntriples -m config/production_mapping_Datenbankurs_Verbaende.ttl -o target/mapping_12.nt
+java -jar lib/RML-Processor-0.3x100.jar -f ntriples -m config/production_mapping_Datenbankurs_VorfasnachtRevueKulturnaechte.ttl -o target/mapping_13.nt
+java -jar lib/RML-Processor-0.3x100.jar -f ntriples -m config/production_mapping_Datenbankurs_Zirkus.ttl -o target/mapping_14.nt
+java -jar lib/RML-Processor-0.3x100.jar -f ntriples -m config/production_mapping_Plakat_alleAttribute.ttl -o target/mapping_15.nt
+java -jar lib/RML-Processor-0.3x100.jar -f ntriples -m config/production_mapping_RepGesamt_alleAttributeohneDokumente.ttl -o target/mapping_16.nt
+java -jar lib/RML-Processor-0.3x100.jar -f ntriples -m config/production_mapping_StammdatenSpielzeit.ttl -o target/mapping_17.nt
+java -jar lib/RML-Processor-0.3x100.jar -f ntriples -m config/production_mapping_ZSV.ttl -o target/mapping_18.nt
+java -jar lib/RML-Processor-0.3x100.jar -f ntriples -m config/production_mapping_audio.ttl -o target/mapping_19.nt
+java -jar lib/RML-Processor-0.3x100.jar -f ntriples -m config/production_mapping_stc_professional.ttl -o target/mapping_20.nt
