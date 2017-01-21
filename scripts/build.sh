c02f22a4f6830577a0880ff717eb3c4c64ca3f7f
Adrian Gschwend <ktk@netlabs.org>
2017-01-21 06:12:34 +0100

    Initial script-setup

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,9 @@ then
   export FUSEKI_HOST="localhost"
 fi
 source ./scripts/init.sh
+source ./scripts/rml.sh
+cat target/mapping_*.nt > target/complete-mapped.nt
 source ./scripts/fuseki-init.sh
 source ./scripts/fuseki-construct.sh
 #cat target/*.nt | sed '/www.ech.ch\/xmlns\/eCH-0071\/1\//d' > target/eCH0071-final.nt
-rapper -i guess -c target/complete.nt
+#rapper -i guess -c target/complete.nt
