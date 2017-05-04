81f786e66db26962f610512e414e53bbc5af5b1e
Adrian Gschwend <ktk@netlabs.org>
2017-05-04 15:29:57 +0200

    First few constructs

--- scripts/fuseki-init.sh
+++ scripts/fuseki-init.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-#docker run -p 3030:3030 -e ADMIN_PASSWORD=cinderella -d stain/jena-fuseki
+#docker run -p 3030:3030 --name fuseki-spa -e ADMIN_PASSWORD=cinderella -d stain/jena-fuseki
 #sleep 20
+source scripts/env.sh
 curl -u admin:cinderella --data "dbType=tdb&dbName=spa"  http://$FUSEKI_HOST:3030/$/datasets
-#s-update --service http://localhost:3030/spa/update 'CLEAR default'
-curl -X PUT  -u admin:cinderella -d @target/complete-mapped.nt -H "Content-Type: application/n-triples" http://admin:cinderella@$FUSEKI_HOST:3030/spa/data
+curl -X PUT  -u admin:cinderella -d @target/20161117_xmlexport_dokumentation.jsonld -H "Content-Type: application/ld+json" http://admin:cinderella@$FUSEKI_HOST:3030/spa/data
+curl -X POST  -u admin:cinderella -d @target/20161117_xmlexport_sammlung.jsonld -H "Content-Type: application/ld+json" http://admin:cinderella@$FUSEKI_HOST:3030/spa/data
+curl -X POST  -u admin:cinderella -d @target/2016_11_16_archiv.jsonld -H "Content-Type: application/ld+json" http://admin:cinderella@$FUSEKI_HOST:3030/spa/data
 
