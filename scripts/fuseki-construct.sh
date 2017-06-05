93d2f130569e135adae2e86c1fe19c054d73c806
Adrian Gschwend <ktk@netlabs.org>
2017-06-05 18:18:28 +0200

    Remaining classes

--- scripts/fuseki-construct.sh
+++ scripts/fuseki-construct.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 source scripts/env.sh
 
-FUSEKI_UPDATE=http://$FUSEKI_HOST:3030/uster/update
+FUSEKI_UPDATE=http://$FUSEKI_HOST:3030/spa/update
 function sparqlu { curl -H "Accept: text/turtle" --data-urlencode update@$1 $2 ; }
 
 #echo $FUSEKI_UPDATE
 
 # some INSERTs
 
-sparqlu construct/gujer/address.rq $FUSEKI_UPDATE
+## Classes
+sparqlu construct/Record-Collection.rq $FUSEKI_UPDATE
+sparqlu construct/Record-Recordset.rq $FUSEKI_UPDATE
+sparqlu construct/Record-CulturalHeritageObject.rq $FUSEKI_UPDATE
+sparqlu construct/Record-AnalogCopy.rq $FUSEKI_UPDATE
+sparqlu construct/Record-DigitalResource.rq $FUSEKI_UPDATE
+sparqlu construct/Record-Item.rq $FUSEKI_UPDATE
 
-# and after that cleanup
+## Properties
 
-sparqlu construct/delete-blanknodes.rq $FUSEKI_UPDATE
+
+## SKOS
