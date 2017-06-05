04464c14a2308a1a5f48c2862abe77c61873d11e
Adrian Gschwend <ktk@netlabs.org>
2017-06-05 17:41:24 +0200

    Initial pipeline

--- scripts/fuseki-construct.sh
+++ scripts/fuseki-construct.sh
@@ -8,19 +8,7 @@ function sparqlu { curl -H "Accept: text/turtle" --data-urlencode update@$1 $2 ;
 
 # some INSERTs
 
-## gujer
 sparqlu construct/gujer/address.rq $FUSEKI_UPDATE
-sparqlu construct/gujer/date.rq $FUSEKI_UPDATE
-sparqlu construct/gujer/jpeg.rq $FUSEKI_UPDATE
-sparqlu construct/gujer/thumbs.rq $FUSEKI_UPDATE
-sparqlu construct/gujer/publisher.rq $FUSEKI_UPDATE
-
-## votations
-
-sparqlu construct/votations/person.rq $FUSEKI_UPDATE
-sparqlu construct/votations/creator.rq $FUSEKI_UPDATE
-sparqlu construct/votations/party.rq $FUSEKI_UPDATE
-sparqlu construct/votations/publisher.rq $FUSEKI_UPDATE
 
 # and after that cleanup
 
